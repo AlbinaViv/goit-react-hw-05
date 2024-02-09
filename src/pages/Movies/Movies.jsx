99732b2e69c8d21ba881bc9a6c70fc3cb3083100
Albina Vivdenko <albinka3322@gmail.com>
2024-02-09 23:32:23 +0100

    1

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -4,40 +4,75 @@ import { useEffect, useState } from "react";
 export default function Movies() {
   const [movies, setMovies] = useState([]);
 
-  // useEffect(() => {
-  //   async function FetchData() {
-  //     try {
-  //       const response = await axios.gat("http://localhost:5173/movies");
-  //       setMovies(response.data);
-  //     } catch (error) {}
-  //   }
-  //   FetchData();
-  //   const url = "https://api.themoviedb.org/3/";
+  useEffect(() => {
+    async function FetchData() {
+      try {
+        const response = await axios.gat(
+          "https://api.themoviedb.org/3/account/20977666"
+        );
+        setMovies(response.data);
+      } catch (error) {}
+    }
+    FetchData();
+  }, []);
 
-  //   // const ACCESS_KEY = "954d1df3195be85d0edb2f306ee16292";
+  // useEffect(() => {
+  //   const url = "https://api.themoviedb.org/3/account/20977666";
 
   //   const options = {
   //     headers: {
-  //       Authorization: "Bearer 954d1df3195be85d0edb2f306ee16292",
+  //       Authorization: "Bearer a138309b07f4565abd84a04ed7875785",
   //     },
   //   };
 
   //   axios
   //     .get(url, options)
-  //     .then((response) => console.log(response))
+  //     .then((response) => setMovies(response.data))
   //     .catch((err) => console.error(err));
   // }, []);
 
   return (
     <div>
-      {/* <h2>Movies</h2>
+      <h2>Movies</h2>
       {movies.length > 0 && (
         <ul>
           {movies.map((movies) => (
-            <li key={movies.id}></li>
+            <li key={movies.id}>{movies.title}</li>
           ))}
         </ul>
-      )} */}
+      )}
     </div>
   );
 }
+
+// export default function Movies() {
+//   const [movies, setMovies] = useState([]);
+
+//
+//     const url = "<https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1>";
+
+//     const options = {
+//       headers: {
+//         Authorization: "Bearer 954d1df3195be85d0edb2f306ee16292",
+//       },
+//     };
+
+//     axios
+//       .get(url, options)
+//       .then((response) => setMovies(response))
+//       .catch((err) => console.error(err));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Movies</h2>
+//       {movies.length > 0 && (
+//         <ul>
+//           {movies.map((movies) => (
+//             <li key={movies.id}></li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
